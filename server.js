e47c79eb917b570245815f2269b54e7d6b8d0535
Blu VIII <34254871+BluVolpe@users.noreply.github.com>
2021-05-16 22:37:38 -0400

    Added heroku script

---
+++ server.js
@@ -0,0 +1,23 @@
+// Requiring express/http/fs to be used
+const express = require("express");
+const http = require("http");
+const fs = require("fs");
+
+// create an instance of express
+const app = express();
+// sets an initial port for listener
+const PORT = process.env.PORT || 3000;
+
+// sets up the Express app to handle data parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// LISTENER
+// code to start the server
+app.listen(PORT, () =>
+  console.log(`Listening on PORT: http://localhost:${PORT}`)
+);
